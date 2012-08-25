57855f8e61b344dd5ab4c18493869a5fa2ac4635
Karambir Singh Nain <karambir@BlackEyed1.(none)>
2012-08-26 04:36:17 +0530

    contact views edited

--- mysite/contact/views.py
+++ mysite/contact/views.py
@@ -9,21 +9,20 @@ from django.forms.widgets import *
 from django.core.mail import send_mail, BadHeaderError
 
 def contactview(request):
-		subject = request.POST.get('topic', '')
-		message = request.POST.get('message', '')
-		from_email = request.POST.get('email', '')
+    subject = request.POST.get('topic', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('email', '')
+    c = {'form':ContactForm()}
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['admin@codesters.org'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thankyou/')
+    else:
+        return render_to_response('contacts.html', {'form': ContactForm()})
 
-		if subject and message and from_email:
-		        try:
-					send_mail(subject, message, from_email, ['admin@codesters.org'])
-        		except BadHeaderError:
-            			return HttpResponse('Invalid header found.')
-        		return HttpResponseRedirect('/contact/thankyou/')
-		else:
-			return render_to_response('contacts.html', {'form': ContactForm()})
-	
-		return render_to_response('contacts.html', {'form': ContactForm()},
-			RequestContext(request))
+    return render_to_response('contacts.html', c, context_instance=RequestContext(request))
 
 def thankyou(request):
 		return render_to_response('thankyou.html')
